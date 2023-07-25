e886e1bf419f86d29ec0860c3729e77d715f6012
Yamil Nostrala <ynostrala@gmail.com>
2023-07-25 17:17:54 -0300

    agrego la clase Carrito y las funciones agregar y verificar si esta en carrito

--- js/app.js
+++ js/app.js
@@ -8,6 +8,39 @@ class Producto{
     }
 }
 
+//clase para carrito
+class Carrito{
+    constructor(){
+        this.carrito = [];
+        this.total = 0;
+        this.totalProductos = 0;
+    }
+
+    agregar(producto){
+        //primero analiza si el producto esta en el carrito.
+        let productoEnCarrito = this.estaEnCarrito(producto);
+        if(productoEnCarrito){
+            //si el producto ya esta en el carrito, suma 1 de cantidad
+            productoEnCarrito.cantidad++;
+        }else{
+            //si no está en el carrito, se agrega al mismo.
+            // this.carrito.push(producto)
+            this.carrito.push({...producto, cantidad: 1}) //le agrego al constructor de Producto, un nuevo parametro de cantidad.
+        }
+
+    }
+
+    quitar(){
+
+    }
+    //como parametro el producto.id de la funcion de cargarProducto--> si el id q en el carrito coincide con el id del producto
+    //me muestra como resultado el producto (con el uso del find)
+    //recorre todo el array del carrito con el carrito.find y compara el id.
+    estaEnCarrito({id}){ //desestructuro el objeto y busco solamente el id.
+        return this.carrito.find((producto) => producto.id === id)
+    }
+}
+
 //simula la base de datos con los productos del e-commerce
 class BaseDeDatos{
     constructor(){
@@ -60,6 +93,11 @@ function cargarProductos(){
             const id = Number(boton.dataset.id);
             const producto = bd.registroPorId(id)
             console.log("estas agregar el producto: " + producto.nombre)
+            //invoco la funcion del carrito "agregar" con los datos del producto seleccionado.
+            carrito.agregar(producto);
         })
     }
 }
+
+//objeto carrito
+const carrito = new Carrito();
