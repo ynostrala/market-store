3ab4cb4e1fa2bc8316f20dd853de6cd9901d96a6
Yamil Nostrala <ynostrala@gmail.com>
2023-07-25 18:32:29 -0300

    agrego funcion de quitar elementos del carrito

--- js/app.js
+++ js/app.js
@@ -27,13 +27,21 @@ class Carrito{
             // this.carrito.push(producto)
             this.carrito.push({...producto, cantidad: 1}) //le agrego al constructor de Producto, un nuevo parametro de cantidad.
         }
-        console.log(this.carrito);
         this.listar()
     }
 
-    quitar(){
-
+    quitar(id){
+        const indice = this.carrito.findIndex((producto) => producto.id === id);
+        if(this.carrito[indice].cantidad > 1){
+            this.carrito[indice].cantidad--;
+        }else{
+            this.carrito.splice(indice, 1);
+        }
+        this.listar();
     }
+
+
+
     //como parametro el producto.id de la funcion de cargarProducto--> si el id q en el carrito coincide con el id del producto
     //me muestra como resultado el producto (con el uso del find)
     //recorre todo el array del carrito con el carrito.find y compara el id.
@@ -42,19 +50,36 @@ class Carrito{
     }
 
     listar(){
+        this.total = 0;
+        this.totalProductos = 0;
         divCarrito.innerHTML = "";
         for(const producto of this.carrito){
             divCarrito.innerHTML += `
-                <div class="productoCarrito">
+                <div class="producto">
                     <h2>${producto.nombre}</h2>
-                    <img src="img/${producto.imagen}" width="200"/>
+                    <img src="img/${producto.imagen}" width="50"/>
                     <p>$ ${producto.precio}</p>
                     <p>Cantidad: ${producto.cantidad}</p>
                     <p><a href="#" class="btnQuitar" data-id="${producto.id}">Eliminar</a></p>
                 </div> 
             `;
+            this.total += producto.cantidad * producto.precio;
+            
+            this.totalProductos += producto.cantidad;
         }
+        
+        //creo evento para el boton de "eliminar del carrito"
+        const botonesQuitar = document.querySelectorAll(".btnQuitar")
+        for(const boton of botonesQuitar) {
+            boton.onclick = (event) =>{
+                event.preventDefault();
+                this.quitar(Number(boton.dataset.id));
+            }
+        }
+        spanCantidadProductos.innerHTML = this.totalProductos;
+        spanTotalCarrito.innerHTML = this.total;
     }
+    
 }
 
 //simula la base de datos con los productos del e-commerce
@@ -64,7 +89,6 @@ class BaseDeDatos{
         //cargamos los productos que apareceran siempre en la bd
         this.agregarRegistro(1, "Arroz", 230, "Alimentos", "arroz.png");
         this.agregarRegistro(2, "Galletitas", 350, "Alimentos", "galletitas.png");
-        console.log(this.productos);
     }
 
     agregarRegistro(id, nombre, precio, categoria, imagen){
@@ -86,6 +110,9 @@ const bd = new BaseDeDatos();
 //elementos
 const divProductos = document.querySelector("#productos");
 const divCarrito = document.querySelector("#carrito");
+const spanCantidadProductos = document.querySelector("#cantidadProductos");
+const spanTotalCarrito = document.querySelector("#totalCarrito");
+
 
 cargarProductos();
 
@@ -93,13 +120,13 @@ function cargarProductos(){
     const productos = bd.traerRegistros();
     for(const producto of productos){
         divProductos.innerHTML += `
-            <div class="producto">
-                <h2>${producto.nombre}</h2>
-                <img src="img/${producto.imagen}" width="200"/>
-                <p>$ ${producto.precio}</p>
-                <p><a href="#" class="btnAgregar" data-id="${producto.id}">Agregar al carrito</a></p>
-            </div> 
-            `
+        <div class="producto">
+        <h2>${producto.nombre}</h2>
+        <img src="img/${producto.imagen}" width="200"/>
+        <p>$ ${producto.precio}</p>
+        <p><a href="#" class="btnAgregar" data-id="${producto.id}">Agregar al carrito</a></p>
+        </div> 
+        `
     }
     //botones de agregar al carrito
     const botonesAgregar = document.querySelectorAll(".btnAgregar");
@@ -108,11 +135,14 @@ function cargarProductos(){
             event.preventDefault();
             const id = Number(boton.dataset.id);
             const producto = bd.registroPorId(id)
-            console.log("estas agregar el producto: " + producto.nombre)
             //invoco la funcion del carrito "agregar" con los datos del producto seleccionado.
             carrito.agregar(producto);
         })
     }
+}
+
+function quitarProductos(){
+
 }
 
 //objeto carrito
